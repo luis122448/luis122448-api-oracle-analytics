79cb801785ea41348d809014730190733610587f
Louis Ci <136776978+luis12244896@users.noreply.github.com>
2023-07-04 14:04:11 -0500

    disabled verify SSH

--- my-test-project/main.py
+++ my-test-project/main.py
@@ -1,14 +1,34 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from OpenSSL import crypto
 import requests
 import uvicorn
 import os
-import base64
-import ssl
+import urllib3
+# import base64
+# import ssl
+import certifi
 
 app = FastAPI()
 app.title = "App Test"
 app.version = "1.0.0"
+
+
+# Ruta del archivo de entrada y salida
+input_file = "./_.grupotsiperu.com.pe.crt"
+output_file = "./_.grupotsiperu.com.pe.pem"
+
+# Cargar el certificado desde el archivo .crt
+with open(input_file, "rb") as file:
+    crt_data = file.read()
+    cert = crypto.load_certificate(crypto.FILETYPE_PEM, crt_data)
+
+# Guardar el certificado en formato .pem
+with open(output_file, "wb") as file:
+    pem_data = crypto.dump_certificate(crypto.FILETYPE_PEM, cert)
+    file.write(pem_data)
+
+print("Archivo convertido exitosamente.")
 
 class UsuarioAuthConfig(BaseModel):
     ruc: str
@@ -56,16 +76,22 @@ def authConfig(usuario: UsuarioAuthConfig):
     }
 
     # Ruta al archivo del certificado
-    cert_file = "./_.grupotsiperu.com.pe.crt"
+    # cert_file = "./_.grupotsiperu.com.pe.crt"
+    # cert_file = "./_.grupotsiperu.com.pe.pem"
 
-    with open(cert_file, "r") as file:
-        content = file.read()
-        print(content)
+    # with open(cert_file, "r") as file:
+    #     content = file.read()
+    #     print(content)
     # Crear un contexto SSL con el certificado
     # context = ssl.create_default_context(cafile=cert_file)
 
+    # Crear un PoolManager con certificados de confianza
+    http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
     # Realizar la solicitud POST
-    response = requests.post(url, json=data, verify=cert_file)
+    # response = requests.post(url, json=data, verify=cert_file)
+    response = requests.post(url, json=data, verify=False)
+    # response = http.request('POST',url, json=data)
+
 
     # Comprobar el estado de la respuesta
     if response.status_code == 200:
@@ -80,6 +106,7 @@ def authConfig(usuario: UsuarioAuthConfig):
         return {"error": "Error al realizar la solicitud POST"}
 
 if __name__ == '__main__':
+
     uvicorn.run("main:app", host="0.0.0.0",
                 port=int(os.environ.get("PORT", 8000)))
     app.run()
