87423e0fab5583f1c1bdd64b7ed37dd46e34a786
Louis Ci <136776978+luis12244896@users.noreply.github.com>
2023-07-04 15:28:48 -0500

    Refactoring Project

--- api-oracle-analytics/main.py
+++ api-oracle-analytics/main.py
@@ -1,34 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-# from OpenSSL import crypto
 import requests
 import uvicorn
 import os
-import urllib3
-# import base64
-# import ssl
-import certifi
 
 app = FastAPI()
 app.title = "App Test"
 app.version = "1.0.0"
-
-
-# Ruta del archivo de entrada y salida
-# input_file = "./_.grupotsiperu.com.pe.crt"
-# output_file = "./_.grupotsiperu.com.pe.pem"
-
-# # Cargar el certificado desde el archivo .crt
-# with open(input_file, "rb") as file:
-#     crt_data = file.read()
-#     cert = crypto.load_certificate(crypto.FILETYPE_PEM, crt_data)
-
-# # Guardar el certificado en formato .pem
-# with open(output_file, "wb") as file:
-#     pem_data = crypto.dump_certificate(crypto.FILETYPE_PEM, cert)
-#     file.write(pem_data)
-
-# print("Archivo convertido exitosamente.")
 
 class UsuarioAuthConfig(BaseModel):
     ruc: str
@@ -65,33 +43,17 @@ def authConfig(usuario: UsuarioAuthConfig):
     url = 'https://sweb3.grupotsiperu.com.pe:8035/erp/api/rest/usuario/auth-config'
 
     # Decodificar la clave en base64
-    clave = usuario.clave
     # clave = base64.b64decode(usuario.clave).decode('utf-8')
 
     # Datos del JSON a enviar
     data = {
         "ruc": usuario.ruc,
         "coduser": usuario.coduser,
-        "clave": clave
+        "clave": usuario.clave
     }
 
-    # Ruta al archivo del certificado
-    # cert_file = "./_.grupotsiperu.com.pe.crt"
-    # cert_file = "./_.grupotsiperu.com.pe.pem"
-
-    # with open(cert_file, "r") as file:
-    #     content = file.read()
-    #     print(content)
-    # Crear un contexto SSL con el certificado
-    # context = ssl.create_default_context(cafile=cert_file)
-
-    # Crear un PoolManager con certificados de confianza
-    http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
     # Realizar la solicitud POST
-    # response = requests.post(url, json=data, verify=cert_file)
     response = requests.post(url, json=data, verify=False)
-    # response = http.request('POST',url, json=data)
-
 
     # Comprobar el estado de la respuesta
     if response.status_code == 200:
