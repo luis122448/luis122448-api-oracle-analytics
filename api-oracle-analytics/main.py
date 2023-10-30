f68a373c6216df793948d5e61f80911c7ba44eaf
luis122448 <luis122448@gmail.com>
2023-10-30 12:36:57 -0500

    Add CORS Configuration

--- api-oracle-analytics/main.py
+++ api-oracle-analytics/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.etl_router import etl_router
 from routers.auth_router import auth_router
 import uvicorn
@@ -12,8 +13,17 @@ app.docs_url = "/docs"
 app.include_router(etl_router)
 app.include_router(auth_router)
 
+# Configura CORS para permitir todas las solicitudes desde cualquier origen (ajusta según tus necesidades)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 if __name__ == '__main__':
     uvicorn.run("main:app",
                 host="0.0.0.0",
                 port=int(os.environ.get("PORT", 8000)),
                 reload=True)
+    
