0618819bea4188a116aab5dee2fcc1005c23da36
luis122448 <luis122448@gmail.com>
2023-10-30 12:51:41 -0500

    Add Integer ID-CIA

--- api-oracle-analytics/schemas/elt_schema.py
+++ api-oracle-analytics/schemas/elt_schema.py
@@ -2,14 +2,14 @@ from pydantic import BaseModel
 from typing import Optional
 
 class EtlComplex(BaseModel):
-    id_cia: str
+    id_cia: int
     fdesde: Optional[str] = None
     fhasta: Optional[str] = None
 
     model_config = {
         "json_schema_extra": {
             "examples": [{
-                "id_cia": "66",
+                "id_cia": 66,
                 "fdesde": "01/01/20",
                 "fhasta": "01/01/24"
             }]
@@ -17,12 +17,12 @@ class EtlComplex(BaseModel):
     }
 
 class EtlBasic(BaseModel):
-    id_cia: str
+    id_cia: int
     
     model_config = {
         "json_schema_extra": {
             "examples": [{
-                    "id_cia": "66"
+                    "id_cia": 66
                 }]
         }
     }
